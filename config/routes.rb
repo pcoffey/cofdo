a4c48ee0ea169ea97620039478529c5667a276fd
Paul Coffey <pcoffey2@gmail.com>
2016-08-23 22:20:32 +0900

    Re-initialise repo

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :tasks
+  devise_for :users
+  root 'pages#home'
+
+  get 'about' => "pages#about"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
